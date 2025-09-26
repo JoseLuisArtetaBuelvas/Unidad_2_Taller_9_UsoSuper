ec5caf3a846d17b69e4af42be6286290b6de68e5
Zadu <jartetab@unicartagena.edu.co>
2025-09-26 16:51:25 -0500

    Ejericicio 3

---
+++ SuperIncorrecto.java
@@ -0,0 +1,8 @@
+package Unidad_2_Taller_9_UsoSuper;
+
+public class SuperIncorrecto {
+    public static void main(String[] args){
+        // Error super.especie = "Aves";
+        //Esto da error porque es un atributo privado Animal.adn;
+    }
+}
